26b027e11d0123eaf61b2ce0dd541e834d289dd3
Neil Holcomb <nholcomb@autofi.io>
2023-02-02 18:51:31 -0800

    updated to include more details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,24 +20,54 @@ export default function Home() {
           />
         </a>
       </div>
-      <h3 className={styles.title}>CVGPC Show #2 Volunteer sign up&apos;s</h3>
+
+      <h1>WE ARE CO-HOSTING THE NEXT SHOW!!!</h1>
+      <h2>CVGPC Show #2 </h2>
+
       <a
         className={styles.link}
         href="https://docs.google.com/spreadsheets/d/1IVxUoQHW6ubHl9XigZAjDt-Imnn1JuaZIZJDGJdOrVQ/edit?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Sign up to volunteer
+        <h3>Lots of Volunteers Needed</h3>
       </a>
       <br />
+
+      <p className={styles.description}>Where: Johansen High School</p>
+      <p className={styles.description}>When: Saturday, February 25th</p>
+      <p className={styles.description}>Time: 8am-7pm ALL DAY EVENT</p>
+      <p className={styles.finePrint}>
+        * We will update times once the schdule for show 2 is posted
+      </p>
+
+      <hr />
+
+      <h3>
+        Part of hosting is supplying a hospitality room for the judges and
+        coaches
+      </h3>
+      <h3>This year we will be prividing breakfast, a taco bar for lunch.</h3>
       <a
         className={styles.link}
         href="https://docs.google.com/spreadsheets/d/1IVxUoQHW6ubHl9XigZAjDt-Imnn1JuaZIZJDGJdOrVQ/edit?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Bring food for hospitality room
+        Click here for items needed and a signup sheet
       </a>
+
+      <hr />
+
+      <h2>
+        Any questions- text Neil Holcomb @ <a href="tel:2093410269"></a>
+        (209)341-0269
+      </h2>
+
+      <h1>
+        🤩A Big thank you to the volunteers who made last year’s shows such a
+        success!
+      </h1>
     </main>
   );
 }

cf6a7c88da9e8d34b633725d687d1ab76d78f8ff
Neil Holcomb <nholcomb@autofi.io>
2023-03-22 19:42:47 -0700

    removed hosting info and added facebook link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,54 +21,23 @@ export default function Home() {
         </a>
       </div>
 
-      <h1>WE ARE CO-HOSTING CVGPC Show #2</h1>
+      <h1>Oakdale Music Boosters</h1>
 
-      <h2>Lots of Volunteers Needed</h2>
-      <a
-        href="https://docs.google.com/document/d/1MREqjxmfq2jirAHkgiJ-ZMH-9gVBM0V-x55h1-BZW4k/edit?usp=sharing"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>Click here to sign up</h2>
-      </a>
-      <br />
-
-      <p className={styles.description}>Where: Johansen High School</p>
-      <p className={styles.description}>When: Saturday, February 25th</p>
-      <p className={styles.description}>Time: 8am-7pm ALL DAY EVENT</p>
-      <p className={styles.finePrint}>
-        * We will update times once the schedule for show 2 is posted
-      </p>
-
-      <hr />
-
-      <h3>
-        Part of hosting is supplying a hospitality room for the judges and
-        coaches
-      </h3>
-      <h3>
-        This year we will be providing breakfast, and a taco bar for lunch.
-      </h3>
       <a
         className={styles.link}
-        href="https://docs.google.com/spreadsheets/d/1IVxUoQHW6ubHl9XigZAjDt-Imnn1JuaZIZJDGJdOrVQ/edit?usp=sharing"
+        href="https://www.facebook.com/groups/OMBoosters/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Click here for items needed and a sign up sheet
+        <Image
+          className={styles.logo}
+          src="/fb_icon_325x325.png"
+          alt="Follow us on FaceBook"
+          width={75}
+          height={75}
+          priority
+        />
       </a>
-
-      <hr />
-
-      <h2>
-        Any questions - text Neil Holcomb @ &nbsp;
-        <a href="tel:2093410269">(209)341-0269</a>
-      </h2>
-
-      <h1>
-        🤩A Big thank you to the volunteers who made last year&apos;s shows such
-        a success!
-      </h1>
     </main>
   );
 }
